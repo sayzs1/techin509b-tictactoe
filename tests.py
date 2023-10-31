d214c1b7826fde64ead73979cdaebd03cf7271a7
sayzs1 <147019945+sayzs1@users.noreply.github.com>
2023-10-31 10:04:15 +0000

    update

--- tests.py
+++ tests.py
@@ -13,7 +13,35 @@ class TestLogic(unittest.TestCase):
         self.assertEqual(logic.get_winner(board), 'X')
 
     # TODO: Test all functions from logic.py!
+            # Test case 2: 'O' is the winner
+        board = [
+            ['X', 'O', 'X'],
+            ['O', 'O', 'X'],
+            ['X', 'O', 'X'],
+        ]
+        self.assertEqual(logic.get_winner(board), 'O')
 
+        # Test case 3: No winner (draw)
+        board = [
+            ['X', 'O', 'X'],
+            ['X', 'X', 'O'],
+            ['O', 'X', 'O'],
+        ]
+        self.assertIsNone(logic.get_winner(board))
+
+    def test_make_empty_board(self):
+        # Test that make_empty_board returns a valid empty board
+        board = logic.make_empty_board()
+        self.assertEqual(board, [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
+        ])
+
+    def test_other_player(self):
+        # Test switching 'X' to 'O' and vice versa
+        self.assertEqual(logic.other_player('X'), 'O')
+        self.assertEqual(logic.other_player('O'), 'X')
 
 if __name__ == '__main__':
     unittest.main()
