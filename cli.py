bbb1c2e68bacc7a43c45a174e4449d7c03d63015
sayzs1 <147019945+sayzs1@users.noreply.github.com>
2023-11-14 19:41:28 -0800

    update new feature

--- cli.py
+++ cli.py
@@ -2,42 +2,18 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board, get_winner, other_player, is_board_full
-
+from logic import TicTacToe
 
 # Reminder to check all the tests
 
 if __name__ == '__main__':
-    board = make_empty_board()
-    winner = None
-    current_player = 'O'
-    while winner is None:
-        print(f"Player {current_player}: take a turn!")
+    game = TicTacToe()
 
-        # TODO: Show the board to the user.
-        for row in board:
-            print(row)
+    num_players = int(input("Enter the number of players (1 or 2): "))
 
-        # TODO: Input a move from the player.
-        while True:
-            try:
-                move = input(f'Player {current_player},please enter your next move, ranging from 0 to 2 (format: x,y): ')
-                x, y = map(int, move.split(','))
-                if board[x][y] is None:
-                    break
-                else:
-                    print("This position is already taken. Try another one.")
-            except(IndexError, ValueError):
-                print('Invalid input, please follow the rule.')
-
-        # TODO: Update the board.
-        board[x][y] = current_player
-
-        # TODO: Update who's turn it is.
-        current_player = other_player(current_player)
-        winner = get_winner(board)
-        if winner is None and is_board_full(board):
-            print("It's a draw!")
-            break 
-    if winner:
-      print(f'player{winner} wins!')
+    if num_players == 1:
+        game.play_single_player()
+    elif num_players == 2:
+        game.play_two_players()
+    else:
+        print("Invalid number of players. Please choose 1 or 2.")
