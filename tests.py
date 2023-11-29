ad5a797e5237f2c309cc7744f048d2ae855d29c7
sayzs1 <147019945+sayzs1@users.noreply.github.com>
2023-11-28 23:33:47 -0800

    Update the test file because I have changed the definition and class in cli.py

--- tests.py
+++ tests.py
@@ -1,36 +1,34 @@
 import unittest
-from logic import get_winner
-from cli import TicTacToe
+from cli import TicTacToe, Human, Bot
 
 class TestLogic(unittest.TestCase):
 
     def test_initialize_game_empty_board(self):
         # Test if the game is initialized with an empty board
-        game = TicTacToe()
+        player1 = Human("Player1")
+        player2 = Bot("Bot")
+        game = TicTacToe(player1, player2)
         self.assertEqual(game.board, [
             [None, None, None],
             [None, None, None],
             [None, None, None],
         ])
 
-    def test_assign_players(self):
-        # Test if players are assigned unique pieces X and O
-        game = TicTacToe()
-        player1, player2 = game.current_user, game.other_player()
-        self.assertEqual(player1, 'O')
-        self.assertEqual(player2, 'X')
-
     def test_switch_player(self):
         # Test if switching players works correctly
-        game = TicTacToe()
-        current_player = game.current_user
+        player1 = Human("Player1")
+        player2 = Bot("Bot")
+        game = TicTacToe(player1, player2)
+        current_player = game.current_player
         game.switch_player()
-        switched_player = game.current_user
+        switched_player = game.current_player
         self.assertNotEqual(current_player, switched_player)
 
     def test_is_valid_move(self):
         # Test if valid moves are correctly identified
-        game = TicTacToe()
+        player1 = Human("Player1")
+        player2 = Bot("Bot")
+        game = TicTacToe(player1, player2)
         game.board = [
             ['X', None, 'O'],
             [None, 'X', None],
@@ -41,57 +39,76 @@ class TestLogic(unittest.TestCase):
 
     def test_get_winner_horizontal(self):
         # Test winning condition in a horizontal row
-        board = [
+        player1 = Human("Player1")
+        player2 = Bot("Bot")
+        game = TicTacToe(player1, player2)
+        game.board = [
             ['X', 'X', 'X'],
             ['O', 'O', None],
             [None, None, None],
         ]
-        self.assertEqual(get_winner(board), 'X')
+        game.winner = game.get_winner()
+        self.assertEqual(game.winner, 'X')
 
     def test_get_winner_vertical(self):
         # Test winning condition in a vertical column
-        board = [
+        player1 = Human("Player1")
+        player2 = Bot("Bot")
+        game = TicTacToe(player1, player2)
+        game.board = [
             ['X', 'O', None],
             ['X', 'O', None],
             ['X', None, None],
         ]
-        self.assertEqual(get_winner(board), 'X')
+        game.winner = game.get_winner()
+        self.assertEqual(game.winner, 'X')
 
     def test_get_winner_diagonal(self):
         # Test winning condition in a diagonal line
-        board = [
+        player1 = Human("Player1")
+        player2 = Bot("Bot")
+        game = TicTacToe(player1, player2)
+        game.board = [
             ['O', 'X', 'X'],
             ['O', 'O', None],
             ['X', None, 'O'],
         ]
-        self.assertEqual(get_winner(board), 'O')
+        game.winner = game.get_winner()
+        self.assertEqual(game.winner, 'O')
 
     def test_get_winner_no_winner(self):
         # Test when there is no winner
-        board = [
+        player1 = Human("Player1")
+        player2 = Bot("Bot")
+        game = TicTacToe(player1, player2)
+        game.board = [
             ['X', 'O', 'X'],
             ['O', 'X', 'O'],
             ['O', 'X', 'O'],
         ]
-        self.assertIsNone(get_winner(board))
+        game.winner = game.get_winner()
+        self.assertIsNone(game.winner)
 
     def test_get_winner_empty_board(self):
         # Test when the board is empty, there should be no winner
-        board = [
-            [None, None, None],
-            [None, None, None],
-            [None, None, None],
-        ]
-        self.assertIsNone(get_winner(board))
+        player1 = Human("Player1")
+        player2 = Bot("Bot")
+        game = TicTacToe(player1, player2)
+        game.winner = game.get_winner()
+        self.assertIsNone(game.winner)
 
     def test_get_winner_draw(self):
         # Test when the game ends in a draw
-        board = [
+        player1 = Human("Player1")
+        player2 = Bot("Bot")
+        game = TicTacToe(player1, player2)
+        game.board = [
             ['X', 'O', 'X'],
             ['O', 'X', 'O'],
             ['O', 'X', 'O'],
         ]
-        self.assertIsNone(get_winner(board))
+        game.winner = game.get_winner()
+        self.assertIsNone(game.winner)
 
 
 if __name__ == '__main__':
